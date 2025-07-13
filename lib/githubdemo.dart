6d35a838b63579b7b5ec157de6b28a5028ed01dc
Aman Nazir <amannazir1020@gmail.com>
2025-07-13 03:37:00 -0700

    demo purpose

---
+++ lib/githubdemo.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Githubdemo extends StatefulWidget {
+  const Githubdemo({super.key});
+
+  @override
+  State<Githubdemo> createState() => _GithubdemoState();
+}
+
+class _GithubdemoState extends State<Githubdemo> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
